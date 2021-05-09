792b4588ed5f2207f72a66ea69cf2466cf792b80
Nitin Bisht <3498georgian@gmail.com>
2021-05-09 20:39:54 +0530

    Default UserDetailsService usage using hardcoded values

--- spring-mysql-security/src/main/java/com/bisht/SecurityConfiguration.java
+++ spring-mysql-security/src/main/java/com/bisht/SecurityConfiguration.java
@@ -1,13 +1,30 @@
 package com.bisht;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        /*
+         For JPA there is no out of the box authentication in spring security
+         we have to make our own userDetailService which gonna return the user by UserName
+         As long as we provide our own custom userDetailService with the loadUserByUsername() method overrided by us to our needs.
+         For now we have just hardcoded everything starting from password and roles.
+         */
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
@@ -18,5 +35,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPWD() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
